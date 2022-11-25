930805f1382a00a205004390425a4d182595dfb1
Carlo Valenzuela <gusvalenzuela77@gmail.com>
2022-11-25 15:03:00 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// ArrayChallenges
+
+
+string[] fraudulentCodes = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179", };
+
+foreach (string code in fraudulentCodes)
+{
+    if (code.StartsWith('B'))
+    {
+        Console.WriteLine($"The code starts with B!!!");
+    }
+
+}
+
+
---
+++ Prueba.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicrosoftCourse
+{
+    public class Prueba
+    {
+        Prueba()
+        {
+            int id = 20;
+            string nombre = "Carlo";
+        }
+        public void GetMessage()
+        {
+            Console.WriteLine("Hola, soy un metodo de la clase prueba");
+        }
+        protected void GetMessage2()
+        {
+            Console.WriteLine("Hola, soy un metodo protegido de laz clase prueba");
+        }
+        private void GetMessage3()
+        {
+            Console.WriteLine("Hola, soy un metodo privado de laz clase prueba");
+        }
+
+    }
+}
